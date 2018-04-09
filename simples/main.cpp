63be36f82eb44427f8cedf1633a53c1b4e7ee882
AndresArias09 <leonardoandres.arias09@gmail.com>
2018-04-08 19:17:53 -0500

    Imprimiendo listas de enteros y caracteres paso a paso

--- simples/main.cpp
+++ simples/main.cpp
@@ -105,19 +105,25 @@ int main(int argc, char** argv) {
 	varInt.anadir_inicio(90);
 	varInt.anadir_final(78);
 	varInt.anadir_posicion(80,2);
+	imprimir2(varInt);
 	varInt.eliminar_posicion(varInt.getTam()/2); //Nueva
+	imprimir2(varInt);
 	varInt.anadir_inicio(0);
 	varInt.anadir_posicion(14,4);
 	varInt.anadir_posicion(35,6);
+	imprimir2(varInt);
 	varInt.eliminar_final();  //Nueva
+	imprimir2(varInt);
 	varInt.anadir_final(87);  //Nueva
+	imprimir2(varInt);
 	varInt.eliminarInicio();
 	varInt.eliminar_final();
 	varInt.eliminar_posicion(3);
+	imprimir2(varInt);
 	
 	//Imprimiendo lista
 	
-  imprimir2(varInt);
+  	imprimir2(varInt);
 	cout<<endl<<endl;
 	
 	//Lista de flotantes
@@ -127,15 +133,21 @@ int main(int argc, char** argv) {
 	varFloat.anadir_inicio(90.8);
 	varFloat.anadir_final(78.06);
 	varFloat.anadir_posicion(80.7,2);
+	imprimir2(varFloat);
 	varFloat.eliminar_posicion(varFloat.getTam()/2); //Nueva
+	imprimir2(varFloat);
 	varFloat.anadir_inicio(0.08);
 	varFloat.anadir_posicion(14.32,4);
 	varFloat.anadir_posicion(35.097,6);
+	imprimir2(varFloat);
 	varFloat.eliminar_final(); //Nueva
+	imprimir2(varFloat);
 	varFloat.anadir_final(87.123); //Nueva
+	imprimir2(varFloat);
 	varFloat.eliminarInicio();
 	varFloat.eliminar_final();
 	varFloat.eliminar_posicion(3);
+	imprimir2(varFloat);
 	
 	//Imprimiendo lista
 	imprimir2(varFloat);
