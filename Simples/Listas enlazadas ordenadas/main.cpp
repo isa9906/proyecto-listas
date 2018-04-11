774fd6d834a1f0678537306f0a7ee33a02cd22bb
AndresArias09 <leonardoandres.arias09@gmail.com>
2018-04-11 13:14:07 -0500

    Ordenando xd la idea es que todo quede ordenado :v

--- Simples/Listas enlazadas ordenadas/main.cpp
+++ Simples/Listas enlazadas ordenadas/main.cpp
@@ -1,35 +1,22 @@
 #include <iostream>
-#include <string>
 #include "Lista.h"
 
-struct estud{
-	long long cod;
-	char *nombre;
-};
-
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
-
-
-
-void imprimir(Lista var){
+void imprimir (Lista var){
 	int i;
 		for(i=0;i<var.getTam();i++)
 		cout<<var.devolverDato(i)<<" ";
 			cout<<endl<<endl;
 }
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char** argv) {
-	//Variables
-	int  i;
-	Lista var;
-	var.insertar('b');
-	imprimir(var);
-	var.insertar('z');
-	imprimir(var);
-	var.insertar('a');
-	imprimir(var);
-	return 0;	
+	Lista prueba;
+	prueba.anadir('c',"esto");
+	prueba.anadir('a',"si");
+	prueba.anadir('b',"funciona");
+	prueba.eliminarInicio();
+	cout<<prueba.getTam()<<endl;
+	prueba.eliminar_final();
+	imprimir(prueba);
+	return 0;
 }
-
