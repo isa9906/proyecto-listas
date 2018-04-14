78d4e27db0eb455365d448a897a733f87b6436ee
AndresArias09 <leonardoandres.arias09@gmail.com>
2018-04-14 16:05:40 -0500

    Menú de multilistas listo

--- Multilistas/Con arreglos/main.cpp
+++ Multilistas/Con arreglos/main.cpp
@@ -2,48 +2,106 @@
 #include <string>
 #include "multilista.h"
 
+int menu(int opcion);
+void menuEliminar(int opcion,int indice);
+void menuListarCarrera(int opcion);
+void menuListarHobbie(int opcion);
 
 using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	switch(menu()){
-		case 1: //Agregar persona
-		break;
-		case 2: // Eliminar
-			menuEliminar();
-		break;
-		case 3: //Listar por edad
-		break;
-		case 4: //Listar por nombre
-		break;
-		case 5: //Listar por hobbie
-		break;
-		case 6: //Listar por carrera
-		break;
-		default: cout<<"Dato erróneo";
+	int c=1,opcion=0,i,numPers=0;
+	while(c!=0){
+		while(!(numPers>0)){ //se valida que el número de personas sea mayor a cero
+			cout<<"Digite el numero de elementos de la lista: ";
+			cin>>numPers;
+			if(!(numPers>0)) cout<<"Dato erróneo, digítelo de nuevo";
+		}
+		multilista lista (numPers,i); //Creación de multilista
+		switch(menu(opcion)){
+				case 1: //Agregar persona
+					if(lista.estaLlena()){
+						cout<<"La lista esta llena, no se pueden agregar nuevos elementos"<<endl;
+					}
+					else{
+						//se agrega una nueva persona
+					}
+				break;
+				case 2: // Eliminar
+					if(lista.estaVacia()){
+						cout<<"La lista esta vacia, no se puede eliminar ningun elemento"<<endl;
+					}
+					else{
+						//se elimina un elemento
+						menuEliminar(opcion,lista.getindice());
+					}
+				break;
+				case 3: //Listar por edad
+					if(lista.estaVacia()){
+						cout<<"La lista esta vacia, no se puede enlistar ningun elemento"<<endl;
+					}
+					else{
+						//se enlista por edad
+					}
+				break;
+				case 4: //Listar por nombre
+					if(lista.estaVacia()){
+						cout<<"La lista esta vacia, no se puede enlistar ningun elemento"<<endl;
+					}
+					else{
+						//se lista por nombre
+						char **cadena = lista.listarByNombre(i);
+						for(i=0;i<lista.getindice();i++){
+							cout<<cadena[i]<<endl;
+						}
+					}
+				break;
+				case 5: //Listar por hobbie
+					if(lista.estaVacia()){
+						cout<<"La lista esta vacia, no se puede enlistar ningun elemento"<<endl;
+					}
+					else{
+						//se enlista por hobbie
+						menuListarHobbie(opcion);
+					}
+				break;
+				case 6: //Listar por carrera
+					if(lista.estaVacia()){
+						cout<<"La lista esta vacia, no se puede enlistar ningun elemento"<<endl;
+					}
+					else{
+						//se enlista por carrera
+						menuListarCarrera(opcion);
+					}
+				break;
+				default: cout<<"Dato erróneo";
+		}
+		cout<<"Para repetir el menu digite cualquier tecla diferente de 0: ";
+		cin>>c;	
+	}
+	
 }
 
-int menu(){
-	cout<<"Digite una opción"<<endl;
-	cout<<"1. Agregar persona"<<endl<<"2. Eliminar"<<"3. Listar por edad"
+int menu(int opcion){
+	cout<<endl<<"Digite una opcion"<<endl;
+	cout<<"1. Agregar persona"<<endl<<"2. Eliminar"<<endl<<"3. Listar por edad"
 	<<endl<<"4. Listar por nombre"<<endl<<"5. Listar por hobbie"<<endl<<"6. Listar por carrera"
-	<<endl<<"Opción: ";
+	<<endl<<"Opcion: ";
 	cin>>opcion;
-	return opcion
+	return opcion;
 }
 
-void menuEliminar(){
+void menuEliminar(int opcion,int indice){
 	int posicion=0;
-	int opcion=0;
-	while(!(posicion>=1 && posicion<=tam)){
+	while(!(posicion>=1 && posicion<=indice)){
 		cout<<endl<<endl<<"Digite el número de lista del elemento que desea eliminar";
-		cin>>posicion;
-		if(!(posicion>=1 && posicion<=tam)) cout<<"Dato erróneo. Inténtelo de nuevo";
+		cin>>posicion; //Posición que se quiere borrar
+		if(!(posicion>=1 && posicion<=indice)) cout<<"Dato erróneo, la lista tiene "<<indice<<" elementos. Inténtelo de nuevo";
 	}
 	cout<<endl<<"Seleccione la lista de la cual desea eliminar el elemento:"<<endl<<"1. Nombre"<<endl<<"2. Carrera"
-	<<endl<<"3. Hobbie"<<"4. Edad"<<endl<<"Opción: ";
+	<<endl<<"3. Hobbie"<<"4. Edad"<<endl<<"Opcion: ";
 	cin>>opcion;
 	switch(opcion){
 		case 1: //Eliminar de la lista nombre
@@ -56,6 +114,40 @@ void menuEliminar(){
 		break;
 		default: cout<<"Dato erróneo";
 	}
-	
 }
+
+void menuListarCarrera(int opcion){
+	cout<<endl<<"Seleccione la carrera de la cual desea enlistar las personas:"<<endl<<"1. Sistemas"<<endl<<"2. Catastral"
+	<<endl<<"3. Electrónica"<<"4. Industrial"<<endl<<"Opcion: ";
+	cin>>opcion;
+	switch(opcion){
+		case 1: //Se enlista por sistemas
+		break;
+		case 2: // Se enlista por castral
+		break;
+		case 3: //Se enlista por electrónica
+		break;
+		case 4: //Se enlista por industrial
+		break;
+		default: cout<<"Dato erróneo";
+	}
+}
+
+void menuListarHobbie(int opcion){
+	cout<<endl<<"Seleccione el hobbie del cual desea enlistar las personas:"<<endl<<"1. Natacion"<<endl<<"2. Basket"
+	<<endl<<"3. Danza"<<"4. Baseball"<<endl<<"Opcion: ";
+	cin>>opcion;
+	switch(opcion){
+		case 1: //Se enlista por natación
+		break;
+		case 2: // Se enlista por basket
+		break;
+		case 3: //Se enlista por danza
+		break;
+		case 4: //Se enlista por baseball
+		break;
+		default: cout<<"Dato erróneo";
+	}
+}
+
 
--- Multilistas/Con arreglos/multilista.h
+++ Multilistas/Con arreglos/multilista.h
@@ -33,18 +33,47 @@ class multilista {
 	private:
 		int *cabecera;
 		Persona *datos;
-		int tam;
+		int tam;        //determina el tamaño limite de personas
+		int indice;		//determinar la posición en la que va el arreglo de personas
+		
+		//se ordena la multilista por nombre, se declara privado
+		void ordenarByNombre (char *nombre,int anterior,int actual){
+			anterior=-1;
+			actual=cabecera[0];
+			while(true){
+				if(actual==0){ //insertar al final
+					datos[anterior-1].signombre=indice+1;
+					break;
+				}
+				else if(strcoll(nombre,datos[actual-1].nombre)==1){
+					anterior=actual;
+					actual=datos[actual-1].signombre;
+				}
+				else {
+					if (anterior==-1){ //agregar a la cabeza
+						datos[indice].signombre = actual;
+						cabecera[0] = indice+1;
+					}
+					else{ //agregar entre medio
+						datos[anterior-1].signombre = indice+1;
+						datos[indice].signombre = actual;
+					}
+					break;
+				}
+			}
+		}
 	public:	
+		//Constructor
 		multilista(int numPers,int i){	
 			cabecera= new int [10];
 			for(i=0;i<10;i++){
 				cabecera[i] = 0;
 			}
 			datos = new Persona [numPers]; 
-			tam=0;
+			indice=0;
 		}
 		void insertar(Persona persona){
-			datos[tam]=persona;	
+			datos[indice]=persona;	
 			if (estaVacia()){
 				//agregando nombre
 				cabecera[0]=1;
@@ -85,14 +114,14 @@ class multilista {
 				int anterior,actual;
 				ordenarByNombre(persona.nombre,anterior,actual);
 			}
-			tam++;
+			indice++;
 		}
 		
 		//retorna una lista de tipo char **
 		char **listarByNombre(int i){
 			int actual;
 			char **lista;
-			lista = new char*[tam];
+			lista = new char*[indice];
 			actual = cabecera[0];
 			i = 0;
 			while(actual!=0){
@@ -102,42 +131,25 @@ class multilista {
 			}
 			return lista;
 		}
-			
-			
-		void ordenarByNombre (char *nombre,int anterior,int actual){
-			anterior=-1;
-			actual=cabecera[0];
-			while(true){
-				if(actual==0){ //insertar al final
-					datos[anterior-1].signombre=tam+1;
-					break;
-				}
-				else if(strcoll(nombre,datos[actual-1].nombre)==1){
-					anterior=actual;
-					actual=datos[actual-1].signombre;
-				}
-				else {
-					if (anterior==-1){ //agregar a la cabeza
-						datos[tam].signombre = actual;
-						cabecera[0] = tam+1;
-					}
-					else{ //agregar entre medio
-						datos[anterior-1].signombre = tam+1;
-						datos[tam].signombre = actual;
-					}
-					break;
-				}
-			}
-		}
-		
 		void eliminar(string nombre, int pos){
 		
 		}
+		//verifica si la lista está vacía
 		bool estaVacia(){
-			if(tam==0){
+			if(indice==0){
 				return true;
 			}
 			return false;
+		}
+		//verifica si la lista está llena
+		bool estaLlena(){
+			if(indice=(tam-1)){
+				return true;
+			}
+			return false;
+		}
+		int getindice(){
+			return indice;
 		}
 		int getTam(){
 			return tam;
