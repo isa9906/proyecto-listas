8226110a66a40e0f31ea312edc07f39465b90bcd
AndresArias09 <leonardoandres.arias09@gmail.com>
2018-04-15 20:11:16 -0500

    Inserción de estudiantes completada

--- Multilistas/Con arreglos/main.cpp
+++ Multilistas/Con arreglos/main.cpp
@@ -6,6 +6,7 @@ int menu(int opcion);
 void menuEliminar(int opcion,int indice);
 void menuListarCarrera(int opcion);
 void menuListarHobbie(int opcion);
+Persona agregarPersona(int opcion);
 
 using namespace std;
 
@@ -13,20 +14,21 @@ using namespace std;
 
 int main() {
 	int c=1,opcion=0,i,numPers=0;
+	while(!(numPers>0)){ //se valida que el número de personas sea mayor a cero
+		cout<<"Digite el numero de elementos de la lista: ";
+		cin>>numPers;
+		if(!(numPers>0)) cout<<"Dato erróneo, digítelo de nuevo";
+	}
+	multilista lista (numPers,i); //Creación de multilista
 	while(c!=0){
-		while(!(numPers>0)){ //se valida que el número de personas sea mayor a cero
-			cout<<"Digite el numero de elementos de la lista: ";
-			cin>>numPers;
-			if(!(numPers>0)) cout<<"Dato erróneo, digítelo de nuevo";
-		}
-		multilista lista (numPers,i); //Creación de multilista
 		switch(menu(opcion)){
 				case 1: //Agregar persona
 					if(lista.estaLlena()){
 						cout<<"La lista esta llena, no se pueden agregar nuevos elementos"<<endl;
 					}
 					else{
-						//se agrega una nueva persona
+						lista.insertar(agregarPersona(opcion));
+						cout<<"Persona insertada"<<endl;
 					}
 				break;
 				case 2: // Eliminar
@@ -148,6 +150,57 @@ void menuListarHobbie(int opcion){
 		break;
 		default: cout<<"Dato erróneo";
 	}
+}
+
+Persona agregarPersona(int opcion){
+	const size_t BUFFER_SIZE = 1024;
+	char *nombre = new char[BUFFER_SIZE];
+	//se agrega una nueva persona
+	Persona persona;
+	//se ingresa el nombre
+	cout<<endl<<"Nombre: ";
+	cin>>nombre;
+	persona.nombre = nombre;			
+	//se ingresa la edad
+	cout<<"Edad: ";
+	cin>>persona.edad;
+	//se ingresa la carrera
+	cout<<"Carrera: 1. Electronica 2. Catastral 3. Industrial 4. Sistemas : ";
+	cin>>opcion;
+	switch(opcion){
+		case 1:					
+			persona.carrera = "electronica";
+		break;
+		case 2:
+			persona.carrera = "catastral";
+		break;
+		case 3:
+			persona.carrera = "industrial";
+		break;
+		case 4:
+			persona.carrera = "sistemas";
+		break;
+		default: cout<<"Dato erroneo";
+	}
+	//se ingresa el hobbie
+	cout<<"Hobbie: 1. Danza 2. Natacion 3. Basket 4. Baseball : ";
+	cin>>opcion;
+	switch(opcion){
+		case 1:
+			persona.hobby = "danza";
+		break;
+		case 2:
+			persona.hobby = "natacion";
+		break;
+		case 3:
+			persona.hobby = "basket";
+		break;
+		case 4:
+			persona.hobby = "baseball";
+		break;
+		default: cout<<"Dato erróneo";
+	}
+	return persona;					
 }
 
 
